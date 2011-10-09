085d1f09ed010d62702e707d409b704f02a5a4d5
Josh London <josh.london@noaa.gov>
2011-10-09 12:12:50 -0700

    INITIAL FUNCTIONALITY FOR *-HISTOS.CSV FILES
    First attempt relied on the ymd_hms() function, but seemed unncessarily slow. Replaced with direct as.POSIXct(). This version sent to J. Womble.

--- MeltHistos.R
+++ MeltHistos.R
@@ -4,14 +4,23 @@
 MeltHistos <- function(d,hist_type="Percent") {
   library(reshape)
   library(lubridate)
+  bins<-data.frame(cbind(variable=paste("Bin",1:24,sep=""),hrs=0:23))
+  bins$hrs<-as.numeric(bins$hrs)
   histos<-read.csv(d)
   histos_sub<-histos[histos$HistType == hist_type,]
   ind <- sapply(histos_sub, is.factor)
   histos_sub[ind] <- lapply(histos_sub[ind], "[", drop=TRUE)
   if(hist_type == 'Percent') {
-    drydata<-per_sub[,c(1,7,16:39)]
-    drydata<-melt(drydata,id=1:2)
+    drydata<-histos_sub[,c(1,7,16:39)]
+    drydata<-melt(drydata,id.vars=1:2)
   }
-  drydata$DateTime <- ymd_hms(drydata$Date)
+  drydata<-merge(drydata,bins)
+  drydata$DateTime<-as.POSIXct(strptime(drydata$Date, "%Y-%m-%d %H:%M:%S",tz="UTC"),tz="UTC") + 
+    dhours(drydata$hrs)
+  drydata$DateTime<-force_tz(drydata$DateTime)
+  drydata<-drydata[,c(2,6,4)]
+  names(drydata)<-c("DeployID","DataDateTime","PercentDry")
+  drydata<-drydata[with(drydata, order(DeployID, DataDateTime)), ]
+  return(drydata)
 }
 
