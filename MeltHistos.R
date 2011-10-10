6cd16434a07606035f18ba4c6bd4db068bed1e0f
Josh London <josh.london@noaa.gov>
2011-10-10 15:51:11 -0700

    ADD DOCUMENTATION AND ORGANIZE
    -Added comments and documentation throughout the function. Should be, eventually, replaced with roxygen documentation.
    
    -Re-organized all the functions into a single if statement for hist_type == 'Percent'. This should allow easier implementation of other hist_types in the future.

--- MeltHistos.R
+++ MeltHistos.R
@@ -1,25 +1,31 @@
-#FUNCTION TO PREP DATA FROM THE *-Histos.csv FILES OUTPUT FROM WC-DAP FOR
-#IMPORT INTO CRAWL AS HAUL-OUT DATA
-
 MeltHistos <- function(d,hist_type="Percent") {
-  library(reshape)
-  library(lubridate)
-  bins<-data.frame(cbind(variable=paste("Bin",1:24,sep=""),hrs=0:23))
-  bins$hrs<-as.numeric(bins$hrs)
-  histos<-read.csv(d)
-  histos_sub<-histos[histos$HistType == hist_type,]
+  # function for 'melting' histos data output from Wildlife Computers' 
+  # WC-DAP program. Stringent requirements exist for *-Histos.csv file 
+  # specified as input.
+  # Notes:
+  # -Format of Date column must be YYYY-MM-DD HH:MM:SS
+  # -CSV file should not be edited.
+  # -Currently, only drytime data (e.g. 'Percent') is supported
+  # -Requires reshape package
+  require(reshape)
+  histos<-read.csv(d)  # read in the specified *-Histos.csv file
+  histos_sub<-histos[histos$HistType == hist_type,] #subset by hist_type
+  # after subsetting, need to drop levels.
   ind <- sapply(histos_sub, is.factor)
   histos_sub[ind] <- lapply(histos_sub[ind], "[", drop=TRUE)
+  # melt and process procedures for 'Percent' drytime histos
   if(hist_type == 'Percent') {
+    bins<-data.frame(cbind(variable=paste("Bin",1:24,sep=""),hrs=0:23))
+    bins$hrs<-as.numeric(bins$hrs)
     drydata<-histos_sub[,c(1,7,16:39)]
     drydata<-melt(drydata,id.vars=1:2)
+    drydata<-merge(drydata,bins)
+    drydata$DateTime<-as.POSIXct(strptime(drydata$Date, "%Y-%m-%d %H:%M:%S",
+                                          tz="UTC"),tz="UTC") + drydata$hrs*3600
+    drydata$DateTime<-force_tz(drydata$DateTime) # force timezone to UTC
+    drydata<-drydata[,c(2,6,4)]
+    names(drydata)<-c("DeployID","DataDateTime","PercentDry")
   }
-  drydata<-merge(drydata,bins)
-  drydata$DateTime<-as.POSIXct(strptime(drydata$Date, "%Y-%m-%d %H:%M:%S",tz="UTC"),tz="UTC") + 
-    drydata$hrs*3600
-  drydata$DateTime<-force_tz(drydata$DateTime)
-  drydata<-drydata[,c(2,6,4)]
-  names(drydata)<-c("DeployID","DataDateTime","PercentDry")
   drydata<-drydata[with(drydata, order(DeployID, DataDateTime)), ]
   return(drydata)
 }
