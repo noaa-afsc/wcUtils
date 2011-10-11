c010c7a886e44a6650616a23cb3a87183d79d1ab
Josh London <josh.london@noaa.gov>
2011-10-10 20:37:54 -0700

    INITIATE ROXYGEN DOCUMENTATION
    Added files and package structure along with roxygen documentation. Using roxygen2 for compatibility with devtools package.

---
+++ wcUtils/R/wcUtils-package.R
@@ -0,0 +1,17 @@
+#' wcUtils: Makes life easier for those working with Wildlife Computers data
+#'
+#' Makes life easier for those working with Wildlife Computers data
+#'
+#' The aim of `wcUtils` is to make your life as an analyst of Wildlife Computers data 
+#' a wee bit easier. You do need a rudimentary understanding of R and the package does 
+#' require the R package 'reshape' (available on CRAN). At this point, the purpose of 
+#' \code{wcUtils} is to make your life easier, not to do the analysis for you. As such, 
+#' the tools focus more on manipulation of data and less on analysis.
+#'
+#' @name wcUtils-package
+#' @aliases wcUtils
+#' @rdname wcUtils-package
+#' @docType package
+#'
+#' @author Josh London \email{josh.london@@noaa.gov}
+NULL
