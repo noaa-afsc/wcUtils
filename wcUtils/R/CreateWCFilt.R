06a86a62e29ab24947bf4649e032424d5e5f3271
Josh London <josh.london@noaa.gov>
2011-10-11 17:12:42 -0700

    ADD CreateWCFilt FUNCTION AND DOCUMENT

--- wcUtils/R/CreateWCFilt.R
+++ wcUtils/R/CreateWCFilt.R
@@ -1,3 +1,25 @@
+#' CreateWCFilt
+#'
+#' Create a *.wcfilt file from a dataframe of deployment information
+#'
+#' The Wildlife Computers' WC-DAP program requires the creation of 'filters'
+#' to organize PTT deployments within an Argos program. These filters can be
+#' created within the WC-DAP GUI interface, but this can be tedious for large
+#' numbers of deployments and there is no method for importing this data. Often
+#' researchers have data on deployments already organized into a spreadsheet,
+#' database or flat file. This function uses the XML package within R to 
+#' process a dataframe into an XML file compatible with the WC-DAP filter
+#' file (*.wcfilt).
+#'
+#' @param tagdata a dataframe with columns 'Name', 'Ptt', and 'Start'
+#' @param projectid a string specifying the name for the project or filter
+#' @param dateformat a string describing the dateformat
+#' @param minclass a string value of either "B","A","0","1","2", or "3"
+#' @return an xml formated output that can be written to a .wcfilt file
+#' @export
+#' @note future plans to have the function accept a destination path and
+#' actually write the output file.
+#'
 CreateWCFilt <- function(tagdata,projectid,
                          dateformat="Y-M-D h:m:s",minclass="B") {
   require(XML)
