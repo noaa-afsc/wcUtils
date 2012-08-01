ce234b7dcf147501253a1b6fa95653a064ba4e5b
Josh London <josh.london@noaa.gov>
2012-07-31 22:17:19 -0700

    ADD Year TO FACET MODEL
    Also fixed the references to 'd' instead of 'drytimes'

--- wcUtils/R/plot_HourlyTimeline.R
+++ wcUtils/R/plot_HourlyTimeline.R
@@ -1,9 +1,10 @@
 plot_HourlyTimeline <- function(d) {
-  d$Hour <- as.POSIXlt(drytimes$DataDateTime,"GMT")$hour
-  drytimes$DayOfMonth <- as.POSIXlt(drytimes$DataDateTime,"GMT")$mday
-  drytimes$Month <- as.POSIXlt(drytimes$DataDateTime,"GMT")$mon+1
-  p<-ggplot(drytimes,aes(DayOfMonth,Hour)) + geom_tile(aes(fill = PercentDry)) + 
+  d$Hour <- as.POSIXlt(d$DataDateTime,"GMT")$hour
+  d$DayOfMonth <- as.POSIXlt(d$DataDateTime,"GMT")$mday
+  d$Month <- as.POSIXlt(d$DataDateTime,"GMT")$mon+1
+  d$Year <- as.POSIXlt(d$DataDateTime,"GMT")$year+1900
+  p<-ggplot(d,aes(DayOfMonth,Hour)) + geom_tile(aes(fill = PercentDry)) + 
     scale_fill_gradient(low = "#F7FBFF", high = "#08306B") + 
-    facet_grid(Month ~ DeployID)
+    facet_grid(Year+Month ~ DeployID)
   print(p)
 }
