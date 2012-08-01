75eafd647701f0432ab5648c7d25e67566e3382e
Josh London <josh.london@noaa.gov>
2012-07-31 22:21:19 -0700

    ADD DOCUMENTATION TO plot_HourlyTimeline.R AND ADD CALL TO REQUIRE ggplot2

--- wcUtils/R/plot_HourlyTimeline.R
+++ wcUtils/R/plot_HourlyTimeline.R
@@ -1,4 +1,14 @@
+#' plot_HourlyTimeline
+#'
+#' Plot hourly timeline data
+#' @param d dataframe object output from MeltHistos function
+#' @return ggplot
+#' @note future plans to evolve this to an autoplot function
+#' @author Josh M London \email{josh.london@@noaa.gov}
+#' @export
+#' @import ggplot2
 plot_HourlyTimeline <- function(d) {
+  require(ggplot2)
   d$Hour <- as.POSIXlt(d$DataDateTime,"GMT")$hour
   d$DayOfMonth <- as.POSIXlt(d$DataDateTime,"GMT")$mday
   d$Month <- as.POSIXlt(d$DataDateTime,"GMT")$mon+1
