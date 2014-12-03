95ae31fa7f1ef36ba074f879844bc289e717e84e
Josh London <josh.london@noaa.gov>
2014-12-02 16:56:59 -0800

    ADD roxygen DOCUMENTATION TO wcProcessGPS

--- R/wcProcessGPS.R
+++ R/wcProcessGPS.R
@@ -1,6 +1,18 @@
-wcProcessGPS <- function(doc) {
+#' Process GPS/fastloc location data
+#' 
+#' \code{wcProcessGPS} determines the GPS/Fastloc tags in need of processing and sends a command to
+#' initiate processing
+#' 
+#' @param xml_content XML content/data returned from wcPOST (with 'action=get_deployments')
+#' 
+#' @return nothing is returned
+wcProcessGPS <- function(xml_content) {
+  if(class(xml_content) == "response") {
+    warning('wcPOST response object provided, extracting content to xml',call.=FALSE)
+    xml_content <- httr::content(xml_content)
+  }
   xpath <- paste("//data/deployment/status[text() = 'needs processing']/../id")
-  ids_to_process <- wcGetIDs(doc,xpath)
+  ids_to_process <- wcGetIDs(xml_content,xpath)
   if(ids_to_process<1) {
     stop("No deployments are in need of GPS processing")
   }
