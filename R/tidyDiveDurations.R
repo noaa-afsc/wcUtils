b51386a473d376d6c3e5b5cb1d038c5dd5245249
Josh London <josh.london@noaa.gov>
2015-06-11 14:54:40 -0700

    NOW SUPPORTS DiveDuration AND TimeAtDepth

---
+++ R/tidyDiveDurations.R
@@ -0,0 +1,37 @@
+#' Apply 'tidy' data principles to the dive-duration histogram data stream
+#' 
+#' \code{tidyDiveDurations} returns a 'tidy'd' data frame of dive duration data
+#' 
+#' The histogram data stream is provided in a 'wide' format (each row represents a time period and the observed values are provided in 1 to 72 'bin' columns). This format can be difficult to work with in R and other data analysis platforms (e.g. database tables), so we use the \code{tidyr} and \code{dplyr} packages to manipulate the data into a more flexible, 'narrow' format. This results in a data structure where every row represents a single observation.
+#' 
+#' This is implemented, here, with the dive duration data. For dive uration data, the tag records the duration in seconds of a qualifying dive and tallies those durationso user-sepcified duration bins and user-specified time bins. This, unlike with timeline data, requires some knowledge of these user-specified bins. As long as the user has uploaded a configuration/report file to the Wildlife Computers Data Portal, then the *-Histos.csv file provides information on the dive durations. If the bin information is not available, the function will produce a warning and output files with generic 'Bin' labels.
+#' 
+#' @param histos a list returned from \code{read_histos}
+#'
+#' @return a data frame with tidy, narrow data structure and actual dive duration bin limits (when provided)
+#' @export
+tidyDiveDurations <- function(histos) {
+  limits <- dplyr::filter(histos$limits,histtype=='DiveDurationLIMITS') %>% 
+    dplyr::select(-histtype) %>% 
+    tidyr::gather(bin,bin_duration_limit,starts_with('bin'))
+  histos <- histos$histos
+  types <- dplyr::group_by(histos,histtype)
+  t <- dplyr::summarise(types, n = n())
+  if (all(t != c('DiveDuration'))) {
+    stop('No DiveDuration data found',call. = FALSE)
+  }
+  if(nrow(limits)<1) {
+    warning("No dive duration limits found. Will use generic bin labels",call.=FALSE)
+  }
+  
+  histos <- dplyr::filter(histos,
+                          histtype=='DiveDuration')
+  diveduration <- histos %>%
+    tidyr::gather(bin,num_dives, starts_with('bin')) %>%
+    dplyr::rename(datadatetime=date) %>% 
+    dplyr::select(one_of(c("deployid","datadatetime","bin","num_dives"))) %>%
+    dplyr::inner_join(limits) %>% 
+    dplyr::select(deployid,datadatetime,num_dives,bin_duration_limit,bin) %>% 
+    dplyr::mutate(bin_duration_limit=format_bins(bin_duration_limit)) %>% 
+    dplyr::arrange(deployid,datadatetime,bin) 
+}
---
+++ R/tidyTimeAtDepth.R
@@ -0,0 +1,37 @@
+#' Apply 'tidy' data principles to the time-at-depth histogram data stream
+#' 
+#' \code{tidyTimeAtDepth} returns a 'tidy'd' data frame of time-at-depth data
+#' 
+#' The histogram data stream is provided in a 'wide' format (each row represents a time period and the observed values are provided in 1 to 72 'bin' columns). This format can be difficult to work with in R and other data analysis platforms (e.g. database tables), so we use the \code{tidyr} and \code{dplyr} packages to manipulate the data into a more flexible, 'narrow' format. This results in a data structure where every row represents a single observation.
+#' 
+#' This is implemented, here, with the time-at-depth data. For time-at-depth data, the tag records the portion of time the tag spent within user-defined dive depth bins. This, unlike with timeline data, requires some knowledge of these user-specified bins. As long as the user has uploaded a configuration/report file to the Wildlife Computers Data Portal, then the *-Histos.csv file provides information on the time-at-depth bins. If the bin information is not available, the function will produce a warning and output files with generic 'Bin' labels.
+#' 
+#' @param histos a list returned from \code{read_histos}
+#'
+#' @return a data frame with tidy, narrow data structure and actual time-at-depth bin limits (when provided)
+#' @export
+tidyTimeAtDepth <- function(histos) {
+  limits <- dplyr::filter(histos$limits,histtype=='TADLIMITS') %>% 
+    dplyr::select(-histtype) %>% 
+    tidyr::gather(bin,bin_depth_limit,starts_with('bin'))
+  histos <- histos$histos
+  types <- dplyr::group_by(histos,histtype)
+  t <- dplyr::summarise(types, n = n())
+  if (all(t != c('TAD'))) {
+    stop('No Time-At-Depth data found',call. = FALSE)
+  }
+  if(nrow(limits)<1) {
+    warning("No TAD limits found. Will use generic bin labels",call.=FALSE)
+  }
+  
+  histos <- dplyr::filter(histos,
+                          histtype=='TAD')
+  diveduration <- histos %>%
+    tidyr::gather(bin,pct_tad, starts_with('bin')) %>%
+    dplyr::rename(datadatetime=date) %>% 
+    dplyr::select(one_of(c("deployid","datadatetime","bin","pct_tad"))) %>%
+    dplyr::inner_join(limits) %>% 
+    dplyr::select(deployid,datadatetime,pct_tad,bin_depth_limit,bin) %>% 
+    dplyr::mutate(bin_depth_limit=format_bins(bin_depth_limit)) %>% 
+    dplyr::arrange(deployid,datadatetime,bin) 
+}
