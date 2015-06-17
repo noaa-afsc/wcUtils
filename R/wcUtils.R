af2f476ee91fc899b4460999ae826b4e3928df4b
Josh London <josh.london@noaa.gov>
2015-06-17 11:11:54 -0700

    ADD wcUtils.R FOR PACKAGE DOCUMENTATION

---
+++ R/wcUtils.R
@@ -0,0 +1,29 @@
+#' wcUtils: A package for import/export of Wildlife Computers tag data
+#'
+#' wcUtils provides functionality for working with data from select 
+#' (SPLASH, SPOT) Wildlife Computers satellite telemetry tags. The package
+#' relies on data files produced by the Wildlife Computers DAP program or
+#' downloaded from the Wildlife Computers Data Portal. Data are read into R
+#' 
+#' 
+#' 
+#' @section wcUtils functions:
+#'   \itemize{
+#'     \item read_behav
+#'     \item read_histos
+#'     \item read_locs
+#'     \item tidyDiveDepths
+#'     \item tidyDiveDurations
+#'     \item tidyTimeAtDepth
+#'     \item tidyTimelines
+#'     \item wcGetDownload
+#'     \item wcGetIDs
+#'     \item wcGetProjectIDs
+#'     \item wcGetRecentIDs
+#'     \item wcPOST
+#'   }
+#' 
+#' @docType package
+#' @name wcUtils
+NULL
+#> NULL
