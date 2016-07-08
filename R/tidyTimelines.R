cdbb4a84699a7ccb9fa0b15fd561425413b8ca6a
Josh London <josh.london@noaa.gov>
2016-07-08 16:55:53 -0700

    ADD all_types OPTION TO RETURN ALL HISTTYPES
    this provides an option for those who programmed tags to transmit both hourly percent and 20-min timelines

--- R/tidyTimelines.R
+++ R/tidyTimelines.R
@@ -7,29 +7,30 @@
 #' This is implemented, here, with the timeline data. For timeline data, tag 'dryness' is provided as either a percentage of each hour the tag was dry or as a binary (1 or 0) value representing whether a tag was dry for a majority of a given 20-minute period. For both of these situations, the values for the 'bin' columns are predictable and we can, in addition to tidying the data structure, also turn the bin values into actual time periods
 #' 
 #' @param histos a list returned from \code{read_histos}
+#' @param all_types by default the dominant histtype is chosen; \code{all_types = TRUE} will return records of all types found
 #' 
 #' @return a data frame with tidy, narrow data structure and actual time periods in place of bins
 #' @export
-tidyTimelines <- function(histos) {
+tidyTimelines <- function(histos,all_types = FALSE) {
     histos <- histos$histos
     histos <- dplyr::filter(histos,
                             histtype %in% c('Percent','1Percent','TwentyMinTimeline'))
     types <- dplyr::group_by(histos,histtype)
-    t <- dplyr::summarise(types, n=n())
+    t <- dplyr::summarise(types, n = n())
     if (all(!t$histtype %in% c('Percent','1Percent','TwentyMinTimeline'))) {
-      stop('No timeline histogram types found',call.=FALSE)
+      stop('No timeline histogram types found',call. = FALSE)
     }
-    if(nrow(t)==1) {
+    if (nrow(t) == 1) {
       type <- as.character(t$histtype[1])
-    } else if(nrow(t)>1) {
-      type <- as.character(subset(t,n=max(n))$histtype[1])
+    } else if (nrow(t) > 1) {
+      type <- as.character(subset(t,n = max(n))$histtype[1])
     }
-    if(type %in% c("Percent","1Percent")) {
+    if (type %in% c("Percent","1Percent") && !all_types) {
       histos_sub <- dplyr::filter(histos,
                                   histtype == type,
                                   lubridate::hour(date) == 0)
-      bins<-list(bin=paste("bin",1:24,sep=""),hour=0:23)
-      bins<-as.data.frame(bins)
+      bins <- list(bin = paste0("bin",1:24),hour = 0:23)
+      bins <- as.data.frame(bins)
       timeline <- histos_sub %>%
         tidyr::gather(bin,percent_dry, starts_with('bin')) %>%
         merge(bins) %>%
@@ -37,19 +38,49 @@ tidyTimelines <- function(histos) {
         dplyr::select(one_of(c("deployid","histtype","datadatetime","percent_dry"))) %>%
         dplyr::arrange(deployid,datadatetime)
     }
-    if(type=="TwentyMinTimeline") {
+    if (type == "TwentyMinTimeline" && !all_types) {
       histos_sub <- dplyr::filter(histos,
                                   histtype == type,
                                   lubridate::hour(date) == 0)
-      bins<-list(bin=paste("bin",1:72,sep=""),secs=seq(from=0,by=1200,length.out=72))
-      bins<-as.data.frame(bins)
+      bins <- list(bin = paste0("bin",1:72),
+                   secs = seq(from = 0,by = 1200,length.out = 72))
+      bins <- as.data.frame(bins)
       timeline <- histos_sub %>%
         tidyr::gather(bin,dry_status,starts_with('bin')) %>%
         merge(bins) %>%
         dplyr::mutate(datadatetime = date + lubridate::seconds(secs),
-                      percent_dry = ifelse(dry_status==0,100,0)) %>%
+                      percent_dry = ifelse(dry_status == 0,100,0)) %>%
         dplyr::select(one_of(c("deployid","histtype","datadatetime","percent_dry"))) %>%
         dplyr::arrange(deployid,datadatetime)
+    }
+    if (all_types) {
+      histos_sub <- dplyr::filter(histos,
+                                  histtype %in% c("Percent","1Percent"),
+                                  lubridate::hour(date) == 0)
+      bins <- list(bin = paste0("bin",1:24),hour = 0:23)
+      bins <- as.data.frame(bins)
+      timeline1 <- histos_sub %>%
+        tidyr::gather(bin,percent_dry, starts_with('bin')) %>%
+        merge(bins) %>%
+        dplyr::mutate(datadatetime = date + lubridate::hours(hour)) %>%
+        dplyr::select(one_of(c("deployid","histtype","datadatetime","percent_dry"))) %>%
+        dplyr::arrange(deployid,datadatetime)
+      
+      histos_sub <- dplyr::filter(histos,
+                                  histtype == "TwentyMinTimeline",
+                                  lubridate::hour(date) == 0)
+      bins <- list(bin = paste0("bin",1:72),
+                   secs = seq(from = 0,by = 1200,length.out = 72))
+      bins <- as.data.frame(bins)
+      timeline2 <- histos_sub %>%
+        tidyr::gather(bin,dry_status,starts_with('bin')) %>%
+        merge(bins) %>%
+        dplyr::mutate(datadatetime = date + lubridate::seconds(secs),
+                      percent_dry = ifelse(dry_status == 0,100,0)) %>%
+        dplyr::select(one_of(c("deployid","histtype","datadatetime","percent_dry"))) %>%
+        dplyr::arrange(deployid,datadatetime)
+      timeline <- dplyr::bind_rows(timeline1,timeline2)
+      
     }
   return(timeline)
 }
