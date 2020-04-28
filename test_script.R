cffa8af3dcf681e92f39a00031387c1cba2ef4aa
Josh.London <josh.london@noaa.gov>
2020-04-28 13:06:35 -0700

    INITIAL ATTEMPT AT BASIC TEST SCRIPT
    will evolve this into a proper testthat testing workflow later

---
+++ test_script.R
@@ -0,0 +1,16 @@
+library(fs)
+library(purrr)
+
+locs_df <- dir_ls(path = here::here('inst'), 
+                  recurse = TRUE,
+                  glob = "*-Locations.csv") %>% 
+  map_df(read_locs, .id = "file")
+
+histos_df <- dir_ls(path = here::here('inst'),
+                    recurse = TRUE,
+                    glob = "*-Histos.csv") %>% 
+  map(read_histos)
+
+test_tad <- tidyTimeAtDepth(histos_df[[6]])
+test_divedepth <- tidyDiveDepths(histos_df[[6]])
+test_diveduration <- tidyDiveDurations(histos_df[[6]])
